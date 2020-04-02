bd6295b839c444d539a5626b458a596a0696498a
Mike Schatz <mschatz@cs.jhu.edu>
2020-04-02 13:55:09 -0400

    update command name

--- run_full_test.sh
+++ run_full_test.sh
@@ -7,5 +7,5 @@ OUTDIR=output_sorted
 #./testdownsampling.sh 500 $OUTDIR
 ./testdownsampling.sh 1000 $OUTDIR
 ./testdownsampling.sh 10000 $OUTDIR
-python heatmap.py snps_sorted.png
-python heatmap.py snps_homo_sorted.png --homozygous
+python variant_heatmap.py snps_sorted.png
+python variant_heatmap.py snps_homo_sorted.png --homozygous
